1a118c13844ad970b4689adc6b517e682505193e
Mac <mac@MacBook-Pro-For-LJ.local>
2016-05-19 16:11:13 +0800

    大头儿子

---
+++ asdasda/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asdasda
+//
+//  Created by Mac on 16/5/19.
+//  Copyright © 2016年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ asdasda/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asdasda
+//
+//  Created by Mac on 16/5/19.
+//  Copyright © 2016年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
